1a91d1662d238e0747f5da0bf36829ff33017ced
LarissaLima97 <larissa.lmaria@gmail.com>
2023-05-18 20:11:28 -0300

    Adding search route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login/login.js";
+import Search from "../pages/Search/search.js";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>} />
+        <Route path='/search' element={<Search/>} />
       </Routes>
     </BrowserRouter>
   );
