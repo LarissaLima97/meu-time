cea680332b3cf9939b03ca75daf768bd6f7223a9
LarissaLima97 <larissa.lmaria@gmail.com>
2023-05-19 17:06:32 -0300

    Creating search page layout

--- src/pages/Search/search.js
+++ src/pages/Search/search.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import DropDown from "../../components/DropDown";
 import Section from "../../components/Section"
 import {MenuItem } from "@mui/material";
@@ -11,39 +12,9 @@ const Search = () => {
             "flag": "https://media-3.api-sports.io/flags/al.svg"
         },
         {
-            "name": "Algeria",
-            "code": "DZ",
-            "flag": "https://media-3.api-sports.io/flags/dz.svg"
-        },
-        {
-            "name": "Andorra",
-            "code": "AD",
-            "flag": "https://media-3.api-sports.io/flags/ad.svg"
-        },
-        {
-            "name": "Azerbaidjan",
-            "code": "AZ",
-            "flag": "https://media-1.api-sports.io/flags/az.svg"
-        },
-        {
             "name": "Bahrain",
             "code": "BH",
             "flag": "https://media-2.api-sports.io/flags/bh.svg"
-        },
-        {
-            "name": "Bangladesh",
-            "code": "BD",
-            "flag": "https://media-1.api-sports.io/flags/bd.svg"
-        },
-        {
-            "name": "Barbados",
-            "code": "BB",
-            "flag": "https://media-2.api-sports.io/flags/bb.svg"
-        },
-        {
-            "name": "Belarus",
-            "code": "BY",
-            "flag": "https://media-3.api-sports.io/flags/by.svg"
         },
         {
             "name": "Belgium",
@@ -61,12 +32,53 @@ const Search = () => {
             "flag": "https://media-2.api-sports.io/flags/bg.svg"
         }];
 
+        const seasons = [2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025];
+
+        const leagues = [{
+            "league": {
+                "id": 4,
+                "name": "Euro Championship",
+                "type": "Cup",
+                "logo": "https://media-2.api-sports.io/football/leagues/4.png"
+            }
+            },
+            {"league": {
+                "id": 158,
+                "name": "Provincial - Luxembourg",
+                "type": "League",
+                "logo": "https://media-3.api-sports.io/football/leagues/158.png"
+            }
+            },
+            {"league": {
+                "id": 555,
+                "name": "Supercupa",
+                "type": "Cup",
+                "logo": "https://media-3.api-sports.io/football/leagues/555.png"
+            }
+            },
+            {"league": {
+                "id": 328,
+                "name": "Esiliiga A",
+                "type": "League",
+                "logo": "https://media-3.api-sports.io/football/leagues/328.png"
+            }},]
+
+        const [selectedCountry, setSelectedCountry] = useState("");
+        const [selectedSeason, setSelectedSeason] = useState("");
+
     return (
         <Section width="70%">
             <h2>Selecione um país:</h2>
-            <DropDown label="País">
+            <DropDown label="País" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
                 {countries.map(country => (
-                    <MenuItem>{country.name}</MenuItem>
+                    <MenuItem key={country.code} value={country.name}>{country.name}</MenuItem>
+                ))}
+            </DropDown>
+
+            <h2>Selecione uma temporada:</h2>
+            <DropDown label="Temporada" value={selectedSeason} onChange={(e) => setSelectedSeason(e.target.value)}>
+                {seasons.map(seasons => (
+                    <MenuItem value={seasons}>{seasons}</MenuItem>
                 ))}
             </DropDown>
         </Section>
