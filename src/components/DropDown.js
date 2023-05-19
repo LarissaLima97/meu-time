0b9df5f528db99cad2d6bd4689851e9b955c02ff
LarissaLima97 <larissa.lmaria@gmail.com>
2023-05-19 17:04:59 -0300

    Adding new properties

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Select, InputLabel,FormControl } from "@mui/material";
 
-export default function DropDown ({children,label}) {
+export default function DropDown ({children,label,value, onChange}) {
     return(
         <FormControl>
             <InputLabel>{label}</InputLabel>
             <Select
             sx={{ width: 300 }}
             label={label}
+            value={value}
+            onChange={onChange}
             >
                 {children}
             </Select>
